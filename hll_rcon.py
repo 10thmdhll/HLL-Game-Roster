f19c04981e83b5ae6c333b82218d0bc7bd2b2a27
10thmdhll <131085434+10thmdhll@users.noreply.github.com>
2025-05-05 12:59:27 -0400

    Update hll_rcon.py

--- hll_rcon.py
+++ hll_rcon.py
@@ -1,27 +1,20 @@
-import socket
+import requests
 
 class RCON:
-    def __init__(self, host, port, password):
-        self.host = host
-        self.port = port
-        self.password = password
-        self.socket = None
+    def __init__(self, host, port, token):
+        self.url = f"http://{host}:{port}/api/players"
+        self.token = token
 
     def __enter__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(5)
-        self.socket.connect((self.host, self.port))
-        self._auth()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.socket:
-            self.socket.close()
-
-    def _auth(self):
-        self.socket.sendall((self.password + "\n").encode())
+        pass  # No cleanup needed
 
     def send_command(self, command):
-        self.socket.sendall((command + "\n").encode())
-        data = self.socket.recv(4096).decode()
-        return data
+        if command.lower() != "players":
+            raise NotImplementedError("Only 'Players' command is supported via API.")
+        headers = {"Authorization": f"Bearer {self.token}"}
+        response = requests.get(self.url, headers=headers)
+        response.raise_for_status()
+        return response.text
